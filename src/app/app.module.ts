1cab69568bd411512942f51ea79bcf73b8791a72
Afshin <af.afshin92@gmail.com>
2022-07-20 10:52:37 +0430

    [Enhancement] Complete header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './header/logo/logo.component';
+import { NavComponent } from './header/nav/nav.component';
+import { IconsComponent } from './header/icons/icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    NavComponent,
+    IconsComponent
   ],
   imports: [
     BrowserModule
